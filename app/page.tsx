b1e7a0bf2feec801585252ee6d87fd1cd57edfde
jorge-r-singh <58617945+jorge-r-singh@users.noreply.github.com>
2025-07-17 07:35:23 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Hello from '../components/Hello';
+
 export default function HomePage() {
   return (
-    <main>
+    <main style={{ padding: '2rem' }}>
       <h1>Video Notes UI</h1>
       <p>App en desarrollo...</p>
+      <Hello />
     </main>
-  )
+  );
 }
