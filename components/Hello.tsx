cf59a569e41f57213027e71d87671287d73cffac
jorge-r-singh <58617945+jorge-r-singh@users.noreply.github.com>
2025-07-17 07:31:12 +0200

    Create Hello.tsx

---
+++ components/Hello.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <div style={{ padding: '1rem', fontSize: '1.25rem' }}>
+      👋 ¡Hola! Este es un componente de prueba funcionando.
+    </div>
+  );
+}
